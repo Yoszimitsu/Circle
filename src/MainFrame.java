60c3bdc4245745b0b1bd02da2caa686cda1b9894
Eryk-C <82085174+Eryk-C@users.noreply.github.com>
2021-04-09 13:52:33 +0200

    Add files via upload
    Inputting parameters from keyboard, increased frames-per-second, reduced circle size

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,8 +1,26 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class MainFrame extends JFrame {
 
-    MainFrame(String title, int nPanel) {
+    public static void main(String[] args) {
+        System.out.println("MULTI-THREAD WITHOUT EXECUTORS");
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter # of threads");
+        int nPanel = keyboard.nextInt();
+        System.out.println("enter # of circles per thread");
+        int nCircle = keyboard.nextInt();
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame frame = new MainFrame("MULTI-THREAD WITHOUT EXECUTORS", nPanel, nCircle);
+            }
+        });
+    }
+
+    MainFrame(String title, int nPanel, int nCircle) {
         super(title);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 500);
@@ -11,7 +29,7 @@ public class MainFrame extends JFrame {
         // Utworzenie N paneli - każdy z nich zajmuje się jednym kółkiem (każdy rysuje osobne)
         DrawPanel[] panels = new DrawPanel[nPanel];
         for(int i=0; i<nPanel; i++)
-            panels[i] = new DrawPanel(getSize(), 10); // Arg nCircle ustala liczbe kółek rysowanych przez pojedynczy JPanel
+            panels[i] = new DrawPanel(getSize(), nCircle, 120); // Arg nCircle ustala liczbe kółek rysowanych przez pojedynczy JPanel
         for(int i=nPanel-1; i>0; i--)
             panels[i-1].add(panels[i]);
         add(panels[0]);
@@ -23,14 +41,4 @@ public class MainFrame extends JFrame {
         setVisible(true);
     }
 
-
-    public static void main(String[] args) {
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                MainFrame frame = new MainFrame("turbo kółka!!!", 100);
-            }
-        });
-    }
 }
